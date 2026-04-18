bb5d6e5615cfe31f13d5ef63626ba1b81ca49fc6
Ziyu-Zhou <zhouziyv15@gmail.com>
2026-04-18 01:15:44 -0700

    updaing final part of block_markdown

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,6 +1,8 @@
 from enum import Enum
 import re
+from htmlnode import ParentNode, LeafNode, HTMLNode
 
+from textnode import TextNode, TextType, text_node_to_html_node
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
     HEADING = "heading"
@@ -86,27 +88,72 @@ def block_to_block_type(block):
     return BlockType.PARAGRAPH
 
 
+# input = "## Hello"
+# input = (
+# "1. First item"
+# "2. Second item"
+# "3. Third item"
+# "4. Fourth item"
+# )
 
 
-
-
-
-
-
-
-
-
-input = "## Hello"
-input = (
-"1. First item"
-"2. Second item"
-"3. Third item"
-"4. Fourth item"
-)
-
-
-result = block_to_block_type(input)
+# result = block_to_block_type(input)
 # print(result)
 
+# helper functions 
 
+# header helper 
+
+def header_helper(markdown):
+    
+    # input is a block of raw markdown 
+    tag = ""
+    text = ""
+    syntax = markdown.strip().split(" ", 1)
+    print(syntax)
+    num = len(syntax[0])
+    tag = f"h{num}"
+    text = syntax[1]
+    print(f"tag: {tag}, text: {text}")
+    # returns the tag and text 
+    return tag, text
+
+
+def markdown_to_html_node(markdown):
+    
+    # 1.
+    blocks = markdown_to_blocks(markdown)
+
+    # 2. 
+    # notes: syntax differ from block type because 
+    # for example: HEADING doesn't tell use #, ## or ###
+
+    # sample: 
+    for block in blocks:
+        block_type = block_to_block_type(block)
+
+        # extract syntax 
+        
+        # code here need to be able to 
+        # 1. parse the header
+        # 2. find a matched tag for this header
+        # 3. 
+
+        if block_type is BlockType.HEADING:
+            tag, text = header_helper(block)
+            
+            ParentNode(p)
+            
+
+        # extract text
+
+
+input = """# My Adventure
+
+This is a **bold** tale of _courage_.
+
+- Found a sword
+- Slew a dragon
+"""
+result = markdown_to_html_node(input)
 
